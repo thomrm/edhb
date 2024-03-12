6963db30423d90f1f8f819602e8315697ed5706e
Thomas Reed-Munoz <thomas@Thoms-MacBook-Pro.local>
2024-03-12 17:29:44 -0400

    Initial commit
    Initial Commander Display
    
    Initial style and placeholder filters
    
    filters
    
    filter style fix
    
    filter tween state
    
    asc & desc list
    
    better sorting asc/desc
    
    sorting types
    
    search initial implementation
    
    prices and releases
    
    pagination initial implementation
    
    pagination fix
    
    loading spinner
    
    filters cleanup
    
    art api
    
    git ignore data
    
    modified gitignore

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+    import '../lib/base.css';
+    import '../lib/colors.css';
+    import { onMount } from 'svelte';
+ 
+    let ready = false;
+
+    onMount(() => {
+        ready = true;
+    });
+</script>
+
+{#if ready}
+    <div class="content-contain">
+        <slot />
+    </div>
+{/if}
+
+<style>
+    :root {
+        & body {
+            background: var(--Background-Color);
+            color: var(--Text-Primary);
+            font-family: 'Nunito Sans', serif;
+            font-weight: 600;
+        }
+
+        & a {
+            color: var(--Primary-Color);
+        }
+    }
+
+    .content-contain {
+        display: flex;
+        flex-direction: column;
+        max-width: 1200px;
+        min-height: 100vh;
+        padding: 0 50px;
+        margin: 0 auto;
+    }
+</style>
